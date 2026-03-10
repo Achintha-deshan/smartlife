9524fe6ab4743f656631393e8a0abf2d8c8eb6a2
Achintha-deshan <achinthadeshan099@gmail.com>
2026-03-10 23:19:39 +0530

    first commit

---
+++ smartlife_backend/src/main/java/edu/lk/ijse/smartlife_backend/SmartlifeBackendApplication.java
@@ -0,0 +1,13 @@
+package edu.lk.ijse.smartlife_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartlifeBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartlifeBackendApplication.class, args);
+    }
+
+}
---
+++ smartlife_backend/src/test/java/edu/lk/ijse/smartlife_backend/SmartlifeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.lk.ijse.smartlife_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartlifeBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
